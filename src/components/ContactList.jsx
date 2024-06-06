21ad0fe589b0fe4d51530ad99671bbdcdd5fb139
reza hagju <ansherlireza2020@gmail.com>
2024-06-06 12:53:45 +0330

    added a loop for clean code inputs and added components folder

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,6 @@
+function ContactList(props) {
+    console.log(props);
+	return <div>ContactList</div>;
+}
+
+export default ContactList;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,20 @@
 import { useState } from "react";
 import styles from "./Form.module.css";
+import ContactList from "./ContactList.jsx";
+const inputs = [
+	{ type: "text", placeholder: "Name", name: "name" },
+	{ type: "text", placeholder: "Last Name", name: "lastName" },
+	{ type: "email", placeholder: "Email", name: "email" },
+	{ type: "number", placeholder: "Phone", name: "phone" },
+];
 function Form() {
+	const [alert, setAlert] = useState("");
+	const [contacts, setContacts] = useState([]);
 	const [data, setData] = useState({
 		name: "",
 		lastName: "",
 		email: "",
-		phone: 0,
+		phone: "",
 	});
 	const changeHandler = (event) => {
 		const value = event.target.value;
@@ -14,45 +23,35 @@ function Form() {
 		setData((data) => ({ ...data, [name]: value }));
 	};
 	const addHandler = () => {
-		console.log(data);
+		if (!data.name || !data.lastName || !data.email || !data.phone) {
+			setAlert("Please Enter Valid Data!");
+			return;
+		}
+		setAlert("");
+		setContacts((contacts) => [...contacts, data]);
+		console.log(contacts);
 	};
 	return (
-		<div className={styles.container}>
+		<div>
 			<div>
-				<input
-					type="text"
-					placeholder="Name"
-					name="name"
-					value={data.name}
-					onChange={changeHandler}
-				/>
-				<input
-					type="text"
-					placeholder="Last Name"
-					name="lastName"
-					value={data.lastName}
-					onChange={changeHandler}
-				/>
+				<div className={styles.container}>
+					{inputs.map((input,index) => (
+						<input
+							key={index}
+							type={input.type}
+							placeholder={input.placeholder}
+							name={input.name}
+							value={data[input.name]}
+							onChange={changeHandler}
+						/>
+					))}
+					<button type="submit" onClick={addHandler}>
+						Add Contact
+					</button>
+				</div>
 			</div>
-			<div>
-				<input
-					type="Email"
-					placeholder="Email"
-					name="email"
-					value={data.email}
-					onChange={changeHandler}
-				/>
-				<input
-					type="number"
-					placeholder="Phone"
-					name="phone"
-					value={data.phone}
-					onChange={changeHandler}
-				/>
-			</div>
-			<button type="submit" onClick={addHandler}>
-				Add Contact
-			</button>
+			<div>{alert && <p>{alert}</p>}</div>
+			<ContactList contacts={contacts} />
 		</div>
 	);
 }
